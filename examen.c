a00d1c45d50905ea2af1b4e60413be80a5d0c74c
AngelPerez10 <111476102+AngelPerez10@users.noreply.github.com>
2022-12-05 11:36:59 -0600

    Update examen.c

--- examen.c
+++ examen.c
@@ -1,90 +1,91 @@
-/**
- * Árboles binarios en C
- * Adolfo Angel Perez Salado
- * 3°D
- * */
-#include <stdio.h>
+/******************************************************************************
+Adolfo Angel Perez Salado
+3°D
+*******************************************************************************/
+
 #include <stdlib.h>
+#include <stdio.h>
 
 struct Nodo {
-    int dato;
-    struct Nodo *izquierda;
-    struct Nodo *derecha;
+	int dato; 
+	struct Nodo* izquierda;
+	struct Nodo* derecha;
+	int nivel_Arbol;
 };
 
 
-struct Nodo *nuevoNodo(int dato) {
-    // Solicitar memoria
-    size_t tamanioNodo = sizeof(struct Nodo);
-    struct Nodo *nodo = (struct Nodo *) malloc(tamanioNodo);
-    // Asignar el dato e iniciar hojas
-    nodo->dato = dato;
-    nodo->izquierda = nodo->derecha = NULL;
-    return nodo;
+
+struct Nodo* nuevoNodo(int dato);
+void eliminar_dato(struct Nodo* arbol_binario);
+void insertar(struct Nodo* raiz_arbol, struct Nodo* Nodo);
+int buscar_dato(struct Nodo* arbol_binario, int dato);
+
+int main() {
+
+    printf("EXAMEN PARCIAL 3\n");
+    printf("*** ARBOL BINARIO *** \n");
+
+    
+	int n = 0, nivel_Arbol = 0;
+	struct Nodo* raiz = nuevoNodo(6);
+	insertar(raiz, nuevoNodo(5));
+	insertar(raiz, nuevoNodo(11));
+	insertar(raiz, nuevoNodo(4));
+	insertar(raiz, nuevoNodo(3));
+	insertar(raiz, nuevoNodo(8));
+	insertar(raiz, nuevoNodo(12));
+	insertar(raiz, nuevoNodo(9));
+	insertar(raiz, nuevoNodo(21));
+	
+	printf("¿QUE NUMERO DESEAS BUSCAR? ");
+	scanf("%d", &n);
+	nivel_Arbol = buscar_dato(raiz, n);
+	if (nivel_Arbol > 0)
+		printf("EL NUMERO ESTA EN EL NIVEL : %d\n", nivel_Arbol);
+	else 
+		printf("LO LAMEENTO EL NUMERO NO SE ENCUENTRA EN EL ARBOL :( \n");
+
+	return 0;
 }
 
-void insertar(struct Nodo *nodo, int dato) {
-    // ¿Izquierda o derecha?
-    // Si es mayor va a la derecha
-    if (dato > nodo->dato) {
-        // Tienes espacio a la derecha?
-        if (nodo->derecha == NULL) {
-            nodo->derecha = nuevoNodo(dato);
-        } else {
-            // Si la derecha ya está ocupada, recursividad ;)
-            insertar(nodo->derecha, dato);
-        }
-    } else {
-        // Si no, a la izquierda
-        if (nodo->izquierda == NULL) {
-            nodo->izquierda = nuevoNodo(dato);
-        } else {
-            // Si la izquierda ya está ocupada, recursividad ;)
-            insertar(nodo->izquierda, dato);
-        }
-    }
+
+struct Nodo* nuevoNodo(int dato) {
+	struct Nodo* Nodo = NULL;
+	Nodo = malloc(sizeof(struct Nodo));
+	Nodo->izquierda = Nodo->derecha = NULL;
+	Nodo->nivel_Arbol = 1;
+	Nodo->dato = dato;
+	return Nodo;
 }
 
-
-void preorden(struct Nodo *nodo) {
-    if (nodo != NULL) {
-        printf("%d,", nodo->dato);
-        preorden(nodo->izquierda);
-        preorden(nodo->derecha);
-    }
+void insertar(struct Nodo* raiz_arbol, struct Nodo* Nodo) {
+	if (raiz_arbol != NULL && Nodo != NULL) {
+		Nodo->nivel_Arbol = raiz_arbol->nivel_Arbol + 1;
+		if (Nodo->dato > raiz_arbol->dato) {
+			if (raiz_arbol->derecha == NULL)
+				raiz_arbol->derecha = Nodo;
+			else
+				insertar(raiz_arbol->derecha, Nodo);
+		} else {
+			if (raiz_arbol->izquierda == NULL)
+				raiz_arbol->izquierda = Nodo;
+			else
+				insertar(raiz_arbol->izquierda, Nodo);
+		}
+	}
 }
 
-void inorden(struct Nodo *nodo) {
-    if (nodo != NULL) {
-        inorden(nodo->izquierda);
-        printf("%d,", nodo->dato);
-        inorden(nodo->derecha);
-    }
-}
-
-void postorden(struct Nodo *nodo) {
-    if (nodo != NULL) {
-        postorden(nodo->izquierda);
-        postorden(nodo->derecha);
-        printf("%d,", nodo->dato);
-    }
-}
-
-int main(void) {
-    struct Nodo *raiz = nuevoNodo(28);
-    insertar(raiz, 11);
-    insertar(raiz, 96);
-    insertar(raiz, 21);
-    insertar(raiz, 6);
-    insertar(raiz, 97);
-    insertar(raiz, 1);
-    insertar(raiz, 30);
-    insertar(raiz, 10);
-    insertar(raiz, 2);
-    printf("\nImprimiendo preorden\n");
-    preorden(raiz);
-    printf("\nImprimiendo inorden\n");
-    inorden(raiz);
-    printf("\nImprimiendo postorden\n");
-    postorden(raiz);
+int buscar_dato(struct Nodo* raiz, int dato) {
+	int l = 0;
+	if (raiz == NULL)
+		return 0;
+	if (raiz->dato == dato)
+		return raiz->nivel_Arbol;
+	l = buscar_dato(raiz->izquierda, dato);
+	if (l > 0)
+		return l;
+	l = buscar_dato(raiz->derecha, dato);
+	if (l > 0)
+		return l;
+	return 0;
 }
